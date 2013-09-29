2976f1cd2a7020a6976d43d6787dea87e3bdf0a5
Andor Polgar <hngrhorace@gmail.com>
2013-09-29 16:59:27 +0200

    Update teletext-helper.js

--- teletext-helper.js
+++ teletext-helper.js
@@ -1,2 +1,81 @@
 var TeletextHelper = {
+  /**
+	 * Alpha Colour Code
+	 * Set the foreground colour of the characters to black and select a G0 set
+	 * as the current default character set.
+	 * @constant
+	 * @static
+	 * @type {number}
+	 */
+	ALPHA_BLACK: 0x00,
+	
+	/**
+	 * Alpha Colour Code
+	 * Set the foreground colour of the characters to red and select a G0 set
+	 * as the current default character set.
+	 * @constant
+	 * @static
+	 * @type {number}
+	 */
+	ALPHA_RED: 0x01,
+
+	/**
+	 * Alpha Colour Code
+	 * Set the foreground colour of the characters to green and select a G0 set
+	 * as the current default character set.
+	 * @constant
+	 * @static
+	 * @type {number}
+	 */
+	ALPHA_GREEN: 0x02,
+
+	/**
+	 * Alpha Colour Code
+	 * Set the foreground colour of the characters to yellow and select a G0
+	 * set as the current default character set.
+	 * @constant
+	 * @static
+	 * @type {number}
+	 */
+	ALPHA_YELLOW: 0x03,
+
+	/**
+	 * Alpha Colour Code
+	 * Set the foreground colour of the characters to blue and select a G0 set
+	 * as the current default character set.
+	 * @constant
+	 * @static
+	 * @type {number}
+	 */
+	ALPHA_BLUE: 0x04,
+
+	/**
+	 * Alpha Colour Code
+	 * Set the foreground colour of the characters to magenta and select a G0
+	 * set as the current default character set.
+	 * @constant
+	 * @static
+	 * @type {number}
+	 */
+	ALPHA_MAGENTA: 0x05,
+
+	/**
+	 * Alpha Colour Code
+	 * Set the foreground colour of the characters to cyan and select a G0 set
+	 * as the current default character set.
+	 * @constant
+	 * @static
+	 * @type {number}
+	 */
+	ALPHA_CYAN: 0x06,
+
+	/**
+	 * Alpha Colour Code
+	 * Set the foreground colour of the characters to white and select a G0 set
+	 * as the current default character set.
+	 * @constant
+	 * @static
+	 * @type {number}
+	 */
+	ALPHA_WHITE: 0x07
 };
